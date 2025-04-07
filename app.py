8dc6714deb75dd3fb65e1a36d1aaad4839780f66
arsikola <123199672+arsikola@users.noreply.github.com>
2025-04-07 12:09:09 +0700

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def wazzup_webhook():
+    print("✅ Получен вебхук от Wazzup:")
+    print(request.json)  # Печать тела запроса в лог
+    return '', 200  # <-- Обязательно возвращаем 200 OK
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Flask-сервер работает! 🚀'
