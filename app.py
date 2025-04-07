cbef8aeb29368d6a5ea4e5b99b5fa4310fd560b6
arsikola <123199672+arsikola@users.noreply.github.com>
2025-04-07 12:31:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def wazzup_webhook():
-    print("✅ Получен вебхук от Wazzup:")
-    print(request.json)  # Печать тела запроса в лог
-    return '', 200  # <-- Обязательно возвращаем 200 OK
+    data = request.json
+    print("📬 Вебхук от Wazzup:")
+    print(data)
+    return '', 200
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Flask-сервер работает! 🚀'
+    return '✅ Сервер работает!', 200
